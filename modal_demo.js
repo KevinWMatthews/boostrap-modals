ef9d19aa5a83f4c0e4c10523ce74ccba2af9def0
Kevin Matthews <kmatthews@cyberdata.net>
2018-03-20 15:16:13 -0700

    Add on click method to button

--- modal_demo.js
+++ modal_demo.js
@@ -5,3 +5,7 @@ $('#demoModal').on('show.bs.modal', function (e) {
 $('#demoModal').on('hidden.bs.modal', function (e) {
     console.log('Modal was hidden');
 })
+
+$('#btnAlert').on('click', function (e) {
+    alert('Coffee!');
+})
